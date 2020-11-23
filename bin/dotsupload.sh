a19c420ea33af6994b6d709961fd36c1f6b7a6ea
devprabal <ghjxvnjd@gmail.com>
2020-11-24 01:24:41 +0530

    safeguard against relative path names used in the script with cp, mkdir commands

--- bin/dotsupload.sh
+++ bin/dotsupload.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+cwd=$(pwd)
+cwd_allowed='/home/devpogi/dotfiles/bin' 
+
+if [[ "$cwd" != ${cwd_allowed} ]]; then
+	echo Run this script from ${cwd_allowed} dir only. # to safeguard against relative paths 
+	exit 1
+fi
+
 for file in $(cat dots)
 do
 	dotdir=${file#\$HOME\/\.}
 	dotdir=${dotdir%\/*}
 	if [[ ${dotdir} =~ .*'/'.* ]]; then
 		mkdir -p ../${dotdir}
-		echo "Copying ${file} to ../${dotdir}/"
+		echo Copying ${file} to ../${dotdir}/
 		eval cp --no-dereference ${file} ../${dotdir}/
 	else
-		echo "Copying ${file} to dotfiles" 
+		echo Copying ${file} to dotfiles 
 		eval cp --no-dereference ${file} ..
 	fi
 done
