23d247af38cf915ea844a470613857243ca48015
devprabal <ghjxvnjd@gmail.com>
2020-11-27 14:52:31 +0530

    checking if the original file exists in home dir

--- bin/dotsupload.sh
+++ bin/dotsupload.sh
@@ -12,13 +12,27 @@ for file in $(cat dots)
 do
 	dotdir=${file#\$HOME\/\.}
 	dotdir=${dotdir%\/*}
+
+	## idk why $HOME prefix in file is not getting expanded
+	file=${file#\$HOME\/}
+	file="$HOME/${file}"
+
 	if [[ ${dotdir} =~ .*'/'.* ]]; then
 		mkdir -p ../${dotdir}
 		echo Copying ${file} to ../${dotdir}/
-		eval cp --no-dereference ${file} ../${dotdir}/
+		if [[ -f ${file} ]]; then
+			cp --no-dereference ${file} ../${dotdir}/
+		else
+			echo "$(tput rev) ${file} does not exist $(tput sgr0)"
+		fi
 	else
 		echo Copying ${file} to dotfiles 
-		eval cp --no-dereference ${file} ..
+		if [[ -f ${file} ]]; then
+			cp --no-dereference ${file} ..
+		else
+			echo "$(tput rev) ${file} does not exist $(tput sgr0)"
+		fi
+
 	fi
 done
 
